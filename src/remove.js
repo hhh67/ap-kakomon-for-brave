9e8990c16798bacc45ed9a39f78b309079ba2e20
hhh67 <hhio.67.2020@gmail.com>
2023-03-05 15:26:02 +0900

    選択肢に対してリスナを設置するようにスクリプトを修正

--- src/remove.js
+++ src/remove.js
@@ -1,6 +1,9 @@
-window.addEventListener('load', () => {
-    const ansbgList = document.getElementsByClassName('ansbg');
-    Array.prototype.forEach.call(ansbgList, (ansbg) => {
-        ansbg.style = '';
+const selectBtnList = document.getElementsByClassName('selectBtn');
+Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
+    selectBtn.addEventListener('click', () => {
+        const ansbgList = document.getElementsByClassName('ansbg');
+        Array.prototype.forEach.call(ansbgList, (ansbg) => {
+            ansbg.style = '';
+        })
     })
 })
