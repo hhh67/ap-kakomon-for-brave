23d6c806b5e072fe5f81859040c749296f2278ad
hhh67 <hhio.67.2020@gmail.com>
2023-03-05 19:59:06 +0900

    #kaisetsu > pを非表示

--- src/remove.js
+++ src/remove.js
@@ -1,9 +1,11 @@
 const selectBtnList = document.getElementsByClassName('selectBtn');
+const ansbgList = document.getElementsByClassName('ansbg');
+const p = document.querySelector('#kaisetsu > p')
 Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
     selectBtn.addEventListener('click', () => {
-        const ansbgList = document.getElementsByClassName('ansbg');
         Array.prototype.forEach.call(ansbgList, (ansbg) => {
             ansbg.style = '';
         })
+        p.style.display = 'none';
     })
 })
