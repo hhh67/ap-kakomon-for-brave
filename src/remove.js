a15d4acc51fcaf52228d438a14e84272fb8a8eb3
hhh67 <hhio.67.2020@gmail.com>
2023-03-06 21:56:22 +0900

    `#kaisetsu > p` をイベント発火時に取得するように

--- src/remove.js
+++ src/remove.js
@@ -1,11 +1,11 @@
 const selectBtnList = document.getElementsByClassName('selectBtn');
 const ansbgList = document.getElementsByClassName('ansbg');
-const p = document.querySelector('#kaisetsu > p')
 Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
     selectBtn.addEventListener('click', () => {
         Array.prototype.forEach.call(ansbgList, (ansbg) => {
             ansbg.style = '';
         })
+        const p = document.querySelector('#kaisetsu > p')
         p.style.display = 'none';
     })
 })
