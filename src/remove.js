2f1bb68453546bab08333d0372059c6813e703b2
hhh67 <hhio.67.2020@gmail.com>
2023-04-15 19:13:38 +0900

    修正

--- src/remove.js
+++ src/remove.js
@@ -1,22 +1,26 @@
 const selectBtnList = document.getElementsByClassName('selectBtn');
 const ansbgList = document.getElementsByClassName('ansbg');
-Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
-    selectBtn.addEventListener('click', () => {
+if (selectBtnList.length !== 0) {
+    Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
+        selectBtn.addEventListener('click', () => {
+            Array.prototype.forEach.call(ansbgList, (ansbg) => {
+                ansbg.style = '';
+            })
+            const p = document.querySelector('#kaisetsu > p');
+            p.style.display = 'none';
+        })
+    })
+}
+
+const answerCheck = document.getElementById('answerCheck');
+if (answerCheck !== null) {
+    answerCheck.addEventListener('click', () => {
         Array.prototype.forEach.call(ansbgList, (ansbg) => {
             ansbg.style = '';
         })
-        const p = document.querySelector('#kaisetsu > p');
-        p.style.display = 'none';
+        const pList = document.querySelectorAll('#kaisetsu > p');
+        Array.prototype.forEach.call(pList, (p) => {
+            p.style.display = 'none';
+        })
     })
-})
-
-const answerCheck = document.getElementById('answerCheck');
-answerCheck.addEventListener('click', () => {
-    Array.prototype.forEach.call(ansbgList, (ansbg) => {
-        ansbg.style = '';
-    })
-    const pList = document.querySelectorAll('#kaisetsu > p');
-    Array.prototype.forEach.call(pList, (p) => {
-        p.style.display = 'none';
-    })
-})
+}
