259349fc7ec4cf0d3de7586af4cad75fe776a1a0
hhh67 <hhio.67.2020@gmail.com>
2024-01-04 13:47:31 +0900

    フォーマット

--- src/remove.js
+++ src/remove.js
@@ -1,19 +1,23 @@
-const selectBtnList = document.getElementsByClassName('selectBtn');
-const ansbgList = document.getElementsByClassName('ansbg');
-if (selectBtnList.length !== 0) {
-    Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
-        selectBtn.addEventListener('click', () => {
-            Array.prototype.forEach.call(ansbgList, (ansbg) => {
-                ansbg.style = '';
-            })
-            const p = document.querySelector('#kaisetsu > p');
-            p.style.display = 'none';
-        })
+window.addEventListener('load', () => {
+    const selectBtnList = document.getElementsByClassName('selectBtn');
+    const ansbgList = document.getElementsByClassName('ansbg');
+    Array.prototype.forEach.call(ansbgList, (ansbg) => {
+        ansbg.classList.remove('R3tfxFm5')
     })
-}
-
-const answerCheck = document.getElementById('answerCheck');
-if (answerCheck !== null) {
+    
+    if (selectBtnList.length !== 0) {
+        Array.prototype.forEach.call(selectBtnList, (selectBtn) => {
+            selectBtn.addEventListener('click', () => {
+                Array.prototype.forEach.call(ansbgList, (ansbg) => {
+                    ansbg.style = '';
+                })
+                const p = document.querySelector('#kaisetsu > p');
+                p.style.display = 'none';
+            })
+        })
+    }
+    
+    const answerCheck = document.getElementById('answerCheck');
     answerCheck.addEventListener('click', () => {
         Array.prototype.forEach.call(ansbgList, (ansbg) => {
             ansbg.style = '';
@@ -23,4 +27,4 @@ if (answerCheck !== null) {
             p.style.display = 'none';
         })
     })
-}
+})
